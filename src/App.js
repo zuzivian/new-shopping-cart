788fc35c8f8f27f0c0b21e4120fdd1a1a90c763f
Nathaniel Wong <NathanielWongKinYew2019@u.northwestern.edu>
2019-01-10 00:47:55 -0600

    cart static render

--- src/App.js
+++ src/App.js
@@ -61,12 +61,84 @@ class Canvas extends Component {
   }
 }
 
+class FloatCart extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      isOpen: true,
+    };
+  }
+
+  render() {
+    let classes = ['float-cart'];
+
+        if (!!this.state.isOpen) {
+          classes.push('float-cart--open');
+        }
+
+        return (
+          <div className={classes.join(' ')}>
+            {/* If cart open, show close (x) button */}
+            {this.state.isOpen && (
+              <div
+                className="float-cart__close-btn"
+              >
+                X
+              </div>
+            )}
+
+            {/* If cart is closed, show bag with quantity of product and open cart action */}
+        {!this.state.isOpen && (
+          <span
+            className="bag bag--float-cart-closed"
+          >
+            <span className="bag__quantity">0</span>
+          </span>
+        )}
+
+        <div className="float-cart__content">
+          <div className="float-cart__header">
+            <span className="bag">
+              <span className="bag__quantity">0</span>
+            </span>
+            <span className="header-title">Cart</span>
+          </div>
+
+          <div className="float-cart__shelf-container">
+              <p className="shelf-empty">
+                Add some products in the cart <br />
+                :)
+              </p>
+          </div>
+
+          <div className="float-cart__footer">
+            <div className="sub">SUBTOTAL</div>
+            <div className="sub-price">
+              <p className="sub-price__val">
+                $0.00
+              </p>
+            </div>
+            <div className="buy-btn">
+              Checkout
+            </div>
+          </div>
+        </div>
+      </div>
+    );
+  }
+
+
+}
+
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <Sidebar />
-        <Canvas />
+        <main>
+          <Sidebar />
+          <Canvas />
+        </main>
+      <FloatCart />
       </div>
     );
   }
